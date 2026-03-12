3f7698af8a2727b83f254dd6b7a07eec159c225e
GloominessEminence <GloominesssEminence@outlook.com>
2026-03-13 07:29:05 +0800

    去掉计算AUC的逻辑，使得DKD可以跑起来。

--- system/flcore/servers/serverbase.py
+++ system/flcore/servers/serverbase.py
@@ -51,7 +51,6 @@ class Server(object):
         self.uploaded_ids = []
 
         self.rs_test_acc = []
-        self.rs_test_auc = []
         self.rs_train_loss = []
 
         self.times = times
@@ -154,7 +153,6 @@ class Server(object):
 
             with h5py.File(file_path, 'w') as hf:
                 hf.create_dataset('rs_test_acc', data=self.rs_test_acc)
-                hf.create_dataset('rs_test_auc', data=self.rs_test_auc)
                 hf.create_dataset('rs_train_loss', data=self.rs_train_loss)
         
         if 'temp' in self.save_folder_name:
@@ -167,17 +165,15 @@ class Server(object):
     def test_metrics(self):
         num_samples = []
         tot_correct = []
-        tot_auc = []
         for c in self.clients:
-            ct, ns, auc = c.test_metrics()
+            ct, ns = c.test_metrics()
             tot_correct.append(ct*1.0)
-            print(f'Client {c.id}: Acc: {ct*1.0/ns}, AUC: {auc}')
-            tot_auc.append(auc*ns)
+            print(f'Client {c.id}: Acc: {ct*1.0/ns}')
             num_samples.append(ns)
 
         ids = [c.id for c in self.clients]
 
-        return ids, num_samples, tot_correct, tot_auc
+        return ids, num_samples, tot_correct
 
     def train_metrics(self):
         num_samples = []
@@ -195,30 +191,17 @@ class Server(object):
     # evaluate selected clients
     def evaluate(self, acc=None, loss=None):
         stats = self.test_metrics()
-        # stats_train = self.train_metrics()
 
         test_acc = sum(stats[2])*1.0 / sum(stats[1])
-        test_auc = sum(stats[3])*1.0 / sum(stats[1])
-        # train_loss = sum(stats_train[2])*1.0 / sum(stats_train[1])
         accs = [a / n for a, n in zip(stats[2], stats[1])]
-        aucs = [a / n for a, n in zip(stats[3], stats[1])]
         
         if acc == None:
             self.rs_test_acc.append(test_acc)
         else:
             acc.append(test_acc)
-        
-        # if loss == None:
-        #     self.rs_train_loss.append(train_loss)
-        # else:
-        #     loss.append(train_loss)
 
-        # print("Averaged Train Loss: {:.4f}".format(train_loss))
         print("Averaged Test Accuracy: {:.4f}".format(test_acc))
-        print("Averaged Test AUC: {:.4f}".format(test_auc))
-        # self.print_(test_acc, train_acc, train_loss)
         print("Std Test Accuracy: {:.4f}".format(np.std(accs)))
-        print("Std Test AUC: {:.4f}".format(np.std(aucs)))
 
     def print_(self, test_acc, test_auc, train_loss):
         print("Average Test Accuracy: {:.4f}".format(test_acc))
