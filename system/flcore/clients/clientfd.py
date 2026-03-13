eb8ab3209bf189388f1f30214f999d3756142837
GloominessEminence <GloominesssEminence@outlook.com>
2026-03-13 21:57:38 +0800

    将蒸馏方法由FEDKD换为FD.

--- system/flcore/clients/clientfd.py
+++ system/flcore/clients/clientfd.py
@@ -1,6 +1,7 @@
 import copy
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 import time
 from flcore.clients.clientbase import Client, load_item, save_item
@@ -13,6 +14,10 @@ class clientFD(Client):
         torch.manual_seed(0)
 
         self.lamda = args.lamda
+        self.distill_type = getattr(args, "distill_type", "KD")
+        self.distill_T = getattr(args, "distill_T", 1.0)
+        self.dkd_alpha = getattr(args, "dkd_alpha", 1.0)
+        self.dkd_beta = getattr(args, "dkd_beta", 1.0)
 
 
     def train(self):
@@ -41,15 +46,64 @@ class clientFD(Client):
                 if self.train_slow:
                     time.sleep(0.1 * np.abs(np.random.rand()))
                 output = model(x)
-                loss = self.loss(output, y)
+                ce_loss = self.loss(output, y)
+                kd_loss = torch.tensor(0.0, device=self.device)
+
+                if self.distill_ratio >= 1.0:
+                    mask = torch.ones_like(y, dtype=torch.float32, device=self.device)
+                else:
+                    mask = (torch.rand_like(y, dtype=torch.float32, device=self.device) < self.distill_ratio).float()
+                if mask.sum() < 1:
+                    rand_idx = torch.randint(0, y.shape[0], (1,), device=self.device)
+                    mask[rand_idx] = 1.0
 
                 if global_logits is not None:
-                    logit_new = copy.deepcopy(output.detach())
+                    teacher_logits = copy.deepcopy(output.detach())
                     for i, yy in enumerate(y):
                         y_c = yy.item()
                         if type(global_logits[y_c]) != type([]):
-                            logit_new[i, :] = global_logits[y_c].data
-                    loss += self.loss(output, logit_new) * self.lamda
+                            teacher_logits[i, :] = global_logits[y_c].data
+
+                    if self.distill_type == "KD":
+                        T = self.distill_T
+                        log_p_s = F.log_softmax(output / T, dim=1)
+                        p_t = F.softmax(teacher_logits / T, dim=1)
+                        kl_per = F.kl_div(log_p_s, p_t, reduction="none").sum(dim=1) * (T * T)
+                        kd_loss = (kl_per * mask).sum() / mask.sum()
+                    elif self.distill_type == "DKD":
+                        T = self.distill_T
+                        alpha = self.dkd_alpha
+                        beta = self.dkd_beta
+                        num_classes = output.size(1)
+                        gt_mask = F.one_hot(y, num_classes=num_classes).bool()
+                        other_mask = ~gt_mask
+
+                        p_s = F.softmax(output / T, dim=1)
+                        p_t = F.softmax(teacher_logits / T, dim=1)
+
+                        p_s_t = (p_s * gt_mask).sum(dim=1, keepdim=True)
+                        p_s_o = (p_s * other_mask).sum(dim=1, keepdim=True)
+                        ps_2 = torch.cat([p_s_t, p_s_o], dim=1)
+
+                        p_t_t = (p_t * gt_mask).sum(dim=1, keepdim=True)
+                        p_t_o = (p_t * other_mask).sum(dim=1, keepdim=True)
+                        pt_2 = torch.cat([p_t_t, p_t_o], dim=1)
+
+                        log_ps_2 = torch.log(ps_2 + 1e-8)
+                        tckd_per = F.kl_div(log_ps_2, pt_2, reduction="none").sum(dim=1) * (T * T)
+
+                        logits_t_part2 = teacher_logits / T - 1000.0 * gt_mask.float()
+                        logits_s_part2 = output / T - 1000.0 * gt_mask.float()
+                        pt_part2 = F.softmax(logits_t_part2, dim=1)
+                        log_ps_part2 = F.log_softmax(logits_s_part2, dim=1)
+                        nckd_per = F.kl_div(log_ps_part2, pt_part2, reduction="none").sum(dim=1) * (T * T)
+
+                        weight = mask / mask.sum()
+                        tckd_loss = (tckd_per * weight).sum()
+                        nckd_loss = (nckd_per * weight).sum()
+                        kd_loss = alpha * tckd_loss + beta * nckd_loss
+
+                loss = ce_loss + self.lamda * kd_loss
 
                 for i, yy in enumerate(y):
                     y_c = yy.item()
@@ -83,16 +137,55 @@ class clientFD(Client):
                     x = x.to(self.device)
                 y = y.to(self.device)
                 output = model(x)
-                loss = self.loss(output, y)
+                ce_loss = self.loss(output, y)
+                kd_loss = torch.tensor(0.0, device=self.device)
 
                 if global_logits is not None:
-                    logit_new = copy.deepcopy(output.detach())
+                    teacher_logits = copy.deepcopy(output.detach())
                     for i, yy in enumerate(y):
                         y_c = yy.item()
                         if type(global_logits[y_c]) != type([]):
-                            logit_new[i, :] = global_logits[y_c].data
-                    loss += self.loss(output, logit_new) * self.lamda
-                    
+                            teacher_logits[i, :] = global_logits[y_c].data
+
+                    if self.distill_type == "KD":
+                        T = self.distill_T
+                        log_p_s = F.log_softmax(output / T, dim=1)
+                        p_t = F.softmax(teacher_logits / T, dim=1)
+                        kd_loss = F.kl_div(log_p_s, p_t, reduction="batchmean") * (T * T)
+                    elif self.distill_type == "DKD":
+                        T = self.distill_T
+                        alpha = self.dkd_alpha
+                        beta = self.dkd_beta
+                        num_classes = output.size(1)
+                        gt_mask = F.one_hot(y, num_classes=num_classes).bool()
+                        other_mask = ~gt_mask
+
+                        p_s = F.softmax(output / T, dim=1)
+                        p_t = F.softmax(teacher_logits / T, dim=1)
+
+                        p_s_t = (p_s * gt_mask).sum(dim=1, keepdim=True)
+                        p_s_o = (p_s * other_mask).sum(dim=1, keepdim=True)
+                        ps_2 = torch.cat([p_s_t, p_s_o], dim=1)
+
+                        p_t_t = (p_t * gt_mask).sum(dim=1, keepdim=True)
+                        p_t_o = (p_t * other_mask).sum(dim=1, keepdim=True)
+                        pt_2 = torch.cat([p_t_t, p_t_o], dim=1)
+
+                        log_ps_2 = torch.log(ps_2 + 1e-8)
+                        tckd_per = F.kl_div(log_ps_2, pt_2, reduction="none").sum(dim=1) * (T * T)
+
+                        logits_t_part2 = teacher_logits / T - 1000.0 * gt_mask.float()
+                        logits_s_part2 = output / T - 1000.0 * gt_mask.float()
+                        pt_part2 = F.softmax(logits_t_part2, dim=1)
+                        log_ps_part2 = F.log_softmax(logits_s_part2, dim=1)
+                        nckd_per = F.kl_div(log_ps_part2, pt_part2, reduction="none").sum(dim=1) * (T * T)
+
+                        tckd_loss = tckd_per.mean()
+                        nckd_loss = nckd_per.mean()
+                        kd_loss = alpha * tckd_loss + beta * nckd_loss
+
+                loss = ce_loss + self.lamda * kd_loss
+
                 train_num += y.shape[0]
                 losses += loss.item() * y.shape[0]
 
